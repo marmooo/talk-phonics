2e4861cacf4d53f75e825f6f69e004136e3a44d8
marmooo <marmooo@users.noreply.github.com>
2021-09-10 09:15:35 +0900

    deno lint

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Talk Phonics
-[フォニックスの発音ゲーム](https://marmooo.github.io/talk-phonics/)です。
-英会話練習にもぴったり。
+
+[フォニックスの発音ゲーム](https://marmooo.github.io/talk-phonics/)です。 英会話練習にもぴったり。
 
 ## Build
+
 ```
 bash build.sh
 ```
-
--- src/index.js
+++ src/index.js
@@ -1,65 +1,47 @@
-const letters = 'ABCDEFGHIJKLMNOPQRSTUVWZXYabcdefghijklmnopqrstuvwxyz';
 let endAudio, incorrectAudio, correctAudio;
 loadAudios();
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
-let voiceInput = setVoiceInput();
+const voiceInput = setVoiceInput();
 let problems = [];
 let problemCandidate;
-let answer = 'Gopher';
+let answer = "Gopher";
 let firstRun = true;
 let englishVoices = [];
 let correctCount = 0;
 
-
 function loadConfig() {
-  if (localStorage.getItem('darkMode') == 1) {
-    document.documentElement.dataset.theme = 'dark';
+  if (localStorage.getItem("darkMode") == 1) {
+    document.documentElement.dataset.theme = "dark";
   }
-  if (localStorage.getItem('voice') == 0) {
-    document.getElementById('voiceOn').classList.add('d-none');
-    document.getElementById('voiceOff').classList.remove('d-none');
+  if (localStorage.getItem("voice") == 0) {
+    document.getElementById("voiceOn").classList.add("d-none");
+    document.getElementById("voiceOff").classList.remove("d-none");
   }
 }
 loadConfig();
 
 function toggleDarkMode() {
-  if (localStorage.getItem('darkMode') == 1) {
-    localStorage.setItem('darkMode', 0);
+  if (localStorage.getItem("darkMode") == 1) {
+    localStorage.setItem("darkMode", 0);
     delete document.documentElement.dataset.theme;
   } else {
-    localStorage.setItem('darkMode', 1);
-    document.documentElement.dataset.theme = 'dark';
+    localStorage.setItem("darkMode", 1);
+    document.documentElement.dataset.theme = "dark";
   }
 }
 
-function toggleVoice(obj) {
-  if (localStorage.getItem('voice') != 0) {
-    localStorage.setItem('voice', 0);
-    document.getElementById('voiceOn').classList.add('d-none');
-    document.getElementById('voiceOff').classList.remove('d-none');
+function toggleVoice() {
+  if (localStorage.getItem("voice") != 0) {
+    localStorage.setItem("voice", 0);
+    document.getElementById("voiceOn").classList.add("d-none");
+    document.getElementById("voiceOff").classList.remove("d-none");
     speechSynthesis.cancel();
   } else {
-    localStorage.setItem('voice', 1);
-    document.getElementById('voiceOn').classList.remove('d-none');
-    document.getElementById('voiceOff').classList.add('d-none');
+    localStorage.setItem("voice", 1);
+    document.getElementById("voiceOn").classList.remove("d-none");
+    document.getElementById("voiceOff").classList.add("d-none");
     speak(answer);
-  }
-}
-
-function toggleEnglish(obj) {
-  if (isEnabled(obj)) {
-    obj.style.opacity = 0.5;
-  } else {
-    obj.style.opacity = 1;
-  }
-}
-
-function isEnabled(obj) {
-  if (obj.style.opacity == 1) {
-    return true;
-  } else {
-    return false;
   }
 }
 
@@ -84,8 +66,8 @@ function unlockAudio() {
 
 function loadAudio(url) {
   return fetch(url)
-    .then(response => response.arrayBuffer())
-    .then(arrayBuffer => {
+    .then((response) => response.arrayBuffer())
+    .then((arrayBuffer) => {
       return new Promise((resolve, reject) => {
         audioContext.decodeAudioData(arrayBuffer, (audioBuffer) => {
           resolve(audioBuffer);
@@ -98,11 +80,11 @@ function loadAudio(url) {
 
 function loadAudios() {
   promises = [
-    loadAudio('mp3/end.mp3'),
-    loadAudio('mp3/incorrect1.mp3'),
-    loadAudio('mp3/correct3.mp3'),
+    loadAudio("mp3/end.mp3"),
+    loadAudio("mp3/incorrect1.mp3"),
+    loadAudio("mp3/correct3.mp3"),
   ];
-  Promise.all(promises).then(audioBuffers => {
+  Promise.all(promises).then((audioBuffers) => {
     endAudio = audioBuffers[0];
     incorrectAudio = audioBuffers[1];
     correctAudio = audioBuffers[2];
@@ -111,19 +93,19 @@ function loadAudios() {
 
 function loadVoices() {
   // https://stackoverflow.com/questions/21513706/
-  const allVoicesObtained = new Promise(function(resolve, reject) {
+  const allVoicesObtained = new Promise(function (resolve) {
     let voices = speechSynthesis.getVoices();
     if (voices.length !== 0) {
       resolve(voices);
     } else {
-      speechSynthesis.addEventListener("voiceschanged", function() {
+      speechSynthesis.addEventListener("voiceschanged", function () {
         voices = speechSynthesis.getVoices();
         resolve(voices);
       });
     }
   });
-  allVoicesObtained.then(voices => {
-    englishVoices = voices.filter(voice => voice.lang == 'en-US');
+  allVoicesObtained.then((voices) => {
+    englishVoices = voices.filter((voice) => voice.lang == "en-US");
   });
 }
 loadVoices();
@@ -131,9 +113,11 @@ loadVoices();
 function speak(text) {
   speechSynthesis.cancel();
   const msg = new SpeechSynthesisUtterance(text);
-  msg.onend = () => { voiceInput.start(); };
+  msg.onend = () => {
+    voiceInput.start();
+  };
   msg.voice = englishVoices[Math.floor(Math.random() * englishVoices.length)];
-  msg.lang = 'en-US';
+  msg.lang = "en-US";
   voiceInput.stop();
   speechSynthesis.speak(msg);
 }
@@ -152,92 +136,96 @@ function nextProblem() {
   if (problemCandidate.length <= 0) {
     problemCandidate = problems.slice();
   }
-  const problem = problemCandidate.splice(getRandomInt(0, problemCandidate.length), 1)[0];
-  var input = document.getElementById('cse-search-input-box-id');
+  const problem =
+    problemCandidate.splice(getRandomInt(0, problemCandidate.length), 1)[0];
+  const input = document.getElementById("cse-search-input-box-id");
   input.value = problem.ja;
   answer = problem.en;
-  const problemObj = document.getElementById('problem');
-  problemObj.innerText = problem.ja + ' (' + problem.en + ')';
-  if (localStorage.getItem('voice') != 0) {
+  const problemObj = document.getElementById("problem");
+  problemObj.innerText = problem.ja + " (" + problem.en + ")";
+  if (localStorage.getItem("voice") != 0) {
     speak(answer);
   }
 }
 
 function initProblems() {
-  var grade = document.getElementById('gradeOption').radio.value;
-  fetch('data/' + grade + '.csv').then(response => response.text()).then(tsv => {
-    problems = [];
-    tsv.split('\n').slice(0, -1).forEach(line => {
-      var [en, ja] = line.split(",");
-      problems.push({ en:en, ja:ja });
-    });
-    problemCandidate = problems.slice();
-  });
+  const grade = document.getElementById("gradeOption").radio.value;
+  fetch("data/" + grade + ".csv").then((response) => response.text()).then(
+    (tsv) => {
+      problems = [];
+      tsv.split("\n").slice(0, -1).forEach((line) => {
+        const [en, ja] = line.split(",");
+        problems.push({ en: en, ja: ja });
+      });
+      problemCandidate = problems.slice();
+    },
+  );
 }
 initProblems();
 
 function searchByGoogle(event) {
   event.preventDefault();
-  var input = document.getElementById('cse-search-input-box-id');
-  var element = google.search.cse.element.getElement('searchresults-only0');
+  const input = document.getElementById("cse-search-input-box-id");
+  const element = google.search.cse.element.getElement("searchresults-only0");
   nextProblem();
-  if (input.value == '') {
+  if (input.value == "") {
     element.clearAllResults();
   } else {
     voiceInput.stop();
     element.execute(input.value);
   }
   if (firstRun) {
-    const gophers = document.getElementById('gophers');
+    const gophers = document.getElementById("gophers");
     while (gophers.firstChild) {
       gophers.removeChild(gophers.lastChild);
     }
     firstRun = false;
   }
-  document.getElementById('reply').textContent = '英語で答えてください';
+  document.getElementById("reply").textContent = "英語で答えてください";
   return false;
 }
-document.getElementById('cse-search-box-form-id').onsubmit = searchByGoogle;
+document.getElementById("cse-search-box-form-id").onsubmit = searchByGoogle;
 
 function setVoiceInput() {
-  if (!('webkitSpeechRecognition' in window)) {
-    document.getElementById('nosupport').classList.remove('d-none');
+  if (!("webkitSpeechRecognition" in window)) {
+    document.getElementById("nosupport").classList.remove("d-none");
   } else {
-    let voiceInput = new webkitSpeechRecognition();
-    voiceInput.lang = 'en-US';
+    const voiceInput = new webkitSpeechRecognition();
+    voiceInput.lang = "en-US";
     // voiceInput.interimResults = true;
     voiceInput.continuous = true;
 
-    voiceInput.onstart = (event) => {
-      const startButton = document.getElementById('start-voice-input');
-      const stopButton = document.getElementById('stop-voice-input');
-      startButton.classList.add('d-none');
-      stopButton.classList.remove('d-none');
+    voiceInput.onstart = () => {
+      document.getElementById("startVoiceInput").classList.add("d-none");
+      document.getElementById("stop-voice-input").classList.remove("d-none");
     };
-    voiceInput.onend = (event) => {
+    voiceInput.onend = () => {
       if (!speechSynthesis.speaking) {
         voiceInput.start();
       }
     };
     voiceInput.onresult = (event) => {
       const reply = event.results[0][0].transcript;
-      const replyObj = document.getElementById('reply');
-      if (reply.toLowerCase().split(' ').includes(answer.toLowerCase())) {
+      const replyObj = document.getElementById("reply");
+      if (reply.toLowerCase().split(" ").includes(answer.toLowerCase())) {
         correctCount += 1;
         playAudio(correctAudio);
         if (correctCount < 5) {
-          replyObj.textContent = '◯ ' + answer;
-          document.getElementById('searchButton').classList.add('animate__animated', 'animate__heartBeat');
+          replyObj.textContent = "◯ " + answer;
+          document.getElementById("searchButton").classList.add(
+            "animate__animated",
+            "animate__heartBeat",
+          );
         } else {
           clearInterval(gameTimer);
           playAudio(endAudio);
-          playPanel.classList.add('d-none');
-          scorePanel.classList.remove('d-none');
-          document.getElementById('score').textContent = correctCount;
+          playPanel.classList.add("d-none");
+          scorePanel.classList.remove("d-none");
+          document.getElementById("score").textContent = correctCount;
         }
       } else {
         playAudio(incorrectAudio);
-        replyObj.textContent = '× ' + reply;
+        replyObj.textContent = "× " + reply;
       }
       voiceInput.stop();
     };
@@ -250,30 +238,28 @@ function startVoiceInput() {
 }
 
 function stopVoiceInput() {
-  const startButton = document.getElementById('start-voice-input');
-  const stopButton = document.getElementById('stop-voice-input');
-  startButton.classList.remove('d-none');
-  stopButton.classList.add('d-none');
-  document.getElementById('reply').textContent = '英語で答えてください';
+  document.getElementById("startVoiceInput").classList.remove("d-none");
+  document.getElementById("stopVoiceInput").classList.add("d-none");
+  document.getElementById("reply").textContent = "英語で答えてください";
   voiceInput.stop();
 }
 
 let gameTimer;
 function startGameTimer() {
   clearInterval(gameTimer);
-  const timeNode = document.getElementById('time');
-  timeNode.innerText = '180秒 / 180秒';
-  gameTimer = setInterval(function() {
-    const arr = timeNode.innerText.split('秒 /');
+  const timeNode = document.getElementById("time");
+  timeNode.innerText = "180秒 / 180秒";
+  gameTimer = setInterval(function () {
+    const arr = timeNode.innerText.split("秒 /");
     const t = parseInt(arr[0]);
     if (t > 0) {
-      timeNode.innerText = (t-1) + '秒 /' + arr[1];
+      timeNode.innerText = (t - 1) + "秒 /" + arr[1];
     } else {
       clearInterval(gameTimer);
       playAudio(endAudio);
-      playPanel.classList.add('d-none');
-      scorePanel.classList.remove('d-none');
-      document.getElementById('score').textContent = correctCount;
+      playPanel.classList.add("d-none");
+      scorePanel.classList.remove("d-none");
+      document.getElementById("score").textContent = correctCount;
     }
   }, 1000);
 }
@@ -281,33 +267,47 @@ function startGameTimer() {
 let countdownTimer;
 function countdown() {
   clearTimeout(countdownTimer);
-  gameStart.classList.remove('d-none');
-  playPanel.classList.add('d-none');
-  scorePanel.classList.add('d-none');
-  const counter = document.getElementById('counter');
+  gameStart.classList.remove("d-none");
+  playPanel.classList.add("d-none");
+  scorePanel.classList.add("d-none");
+  const counter = document.getElementById("counter");
   counter.innerText = 3;
-  countdownTimer = setInterval(function(){
-    const colors = ['skyblue', 'greenyellow', 'violet', 'tomato'];
+  countdownTimer = setInterval(function () {
+    const colors = ["skyblue", "greenyellow", "violet", "tomato"];
     if (parseInt(counter.innerText) > 1) {
       const t = parseInt(counter.innerText) - 1;
       counter.style.backgroundColor = colors[t];
       counter.innerText = t;
     } else {
       clearTimeout(countdownTimer);
-      gameStart.classList.add('d-none');
-      playPanel.classList.remove('d-none');
+      gameStart.classList.add("d-none");
+      playPanel.classList.remove("d-none");
       correctCount = 0;
-      document.getElementById('score').textContent = 0;
-      document.getElementById('searchButton').classList.add('animate__animated', 'animate__heartBeat');
+      document.getElementById("score").textContent = 0;
+      document.getElementById("searchButton").classList.add(
+        "animate__animated",
+        "animate__heartBeat",
+      );
       startGameTimer();
     }
   }, 1000);
 }
 
-
-document.getElementById('searchButton').addEventListener('animationend', function() {
-  this.classList.remove('animate__heartBeat');
+document.getElementById("toggleDarkMode").onclick = toggleDarkMode;
+document.getElementById("toggleVoice").onclick = toggleVoice;
+document.getElementById("restartButton").onclick = countdown;
+document.getElementById("startButton").onclick = countdown;
+document.getElementById("startVoiceInput").onclick = startVoiceInput;
+document.getElementById("stopVoiceInput").onclick = stopVoiceInput;
+document.getElementById("respeak").onclick = respeak;
+document.getElementById("searchButton").addEventListener(
+  "animationend",
+  function () {
+    this.classList.remove("animate__heartBeat");
+  },
+);
+document.getElementById("gradeOption").onchange = initProblems;
+document.addEventListener("click", unlockAudio, {
+  once: true,
+  useCapture: true,
 });
-document.getElementById('gradeOption').onchange = initProblems;
-document.addEventListener('click', unlockAudio, { once:true, useCapture:true });
-
