9af26baa7e8396e8890832463308c2b0f2bee242
marmooo <marmooo@users.noreply.github.com>
2021-09-12 08:45:42 +0900

    bump bootstrap from 5.1.0 to 5.1.1

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2021-09-10 09:15";
+var CACHE_NAME = '2021-09-12 08:36';
 var urlsToCache = [
   "/talk-phonics/",
   "/talk-phonics/index.js",
@@ -6,8 +6,8 @@ var urlsToCache = [
   "/talk-phonics/mp3/incorrect1.mp3",
   "/talk-phonics/mp3/correct3.mp3",
   "/talk-phonics/favicon/original.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
 ];
 
