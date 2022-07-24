aa2976479dd61e577ee250581fc10fef938c8ee8
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:18 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-07-21 08:40";
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/talk-phonics/",
   "/talk-phonics/index.js",
@@ -8,8 +8,8 @@ var urlsToCache = [
   "/talk-phonics/mp3/correct3.mp3",
   "/talk-phonics/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
 ];
 
