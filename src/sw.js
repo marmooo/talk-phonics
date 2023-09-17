223b5313ff7b3be507e4f1dc042e2aac053e0e2d
marmooo <marmooo@users.noreply.github.com>
2023-09-17 09:12:36 +0900

    bump bootstrap from 5.3.1 to 5.3.2

--- src/sw.js
+++ src/sw.js
@@ -8,7 +8,7 @@ const urlsToCache = [
   "/talk-phonics/mp3/correct3.mp3",
   "/talk-phonics/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
 ];
 
 self.addEventListener("install", (event) => {
