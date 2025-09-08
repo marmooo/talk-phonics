1702c1b0b590645ef9dc7941668ea3d138af164c
marmooo <marmooo@users.noreply.github.com>
2025-09-09 00:35:42 +0900

    bump bootstrap from 5.3.7 to 5.3.8

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Collapse } from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/+esm";
+import { Collapse } from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/+esm";
 
 const infoPanel = document.getElementById("infoPanel");
 const playPanel = document.getElementById("playPanel");
