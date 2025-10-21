474097e4869f59179c20e38da8632a22b53bc4ed
marmooo <marmooo@users.noreply.github.com>
2025-10-22 04:00:43 +0900

    fix voice recognition

--- src/index.js
+++ src/index.js
@@ -168,9 +168,6 @@ function loadVoices() {
 function speak(text) {
   speechSynthesis.cancel();
   const msg = new globalThis.SpeechSynthesisUtterance(text);
-  msg.onend = () => {
-    voiceInput.start();
-  };
   msg.voice = englishVoices[Math.floor(Math.random() * englishVoices.length)];
   msg.lang = "en-US";
   voiceInput.stop();
